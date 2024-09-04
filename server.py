cfe4a31376eaff64501c33157d6257bbc5668cd8
me <ditz@ditz>
2024-09-04 18:54:36 -0400

    server changes

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 import asyncio
 import websockets
 
-global num_connections
-
+num_connections = 1
 async def handle_connection(websocket, path):
+    global num_connections
     try:
         print("New connection")
         num_connections += 1
         #send connected id back to player
-        await websocket.send(f"connected: {num_connections}")
+        await websocket.send(f"{num_connections}")
         async for message in websocket:
             print(f"Received message: {message}")
             await websocket.send(message)  # Echoes the received message
@@ -17,11 +17,13 @@ async def handle_connection(websocket, path):
                 break
     except websockets.exceptions.ConnectionClosed as e:
         print(f"Connection closed: {e}")
+        num_connections -= 1
 
 async def main():
     server = await websockets.serve(handle_connection, "192.168.1.42", 9000)
     print("Server started...")
     await server.wait_closed()
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
