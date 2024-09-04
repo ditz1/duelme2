3c7068e44c8c485af6192d07e4685efabb481af8
ditz1 <danielitzler@gmail.com>
2024-09-04 16:17:15 -0400

    added mac build config

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -eq 0 ] || [ "$1" = "native" ]; then
+    mkdir -p build
+    cd build
+    cmake -G "Unix Makefiles" ..
+    make
+    cd ..
+elif [ "$1" = "clean" ]; then
+    rm -rf build
+elif [ "$1" = "web" ]; then
+    mkdir -p build
+    cd build 
+    emcmake cmake -DCMAKE_TOOLCHAIN_FILE="$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" ..
+    emmake make
+    cd ..
+elif [ "$1" = "rebuild" ]; then
+    rm -rf build
+    mkdir build
+    cd build
+    cmake -G "Unix Makefiles" ..
+    make
+    cd ..
+else
+    echo "Invalid argument. Use 'native', 'clean', 'web', or 'rebuild'."
+    exit 1
+fi
